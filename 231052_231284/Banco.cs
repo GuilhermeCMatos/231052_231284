2686fcc75b89501f40dde1d6bc8c6320da51b3d6
Usuario <Usuario@CPD03-M19>
2024-09-24 12:14:42 -0300

    continuando o trabalho

---
+++ 231052_231284/Banco.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data;
+using System.Linq.Expressions;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace _231052_231284
+{
+    public class Banco
+    {
+        // Criando as variáveis publicas para conexão e consulta serão usadas em todo o projeto
+        // connection responsável pela conexão com MySQL
+        public static MySqlConnection Conexao;
+        // Command responsável pelas instruções SQL a serem executadas
+        public static MySqlCommand Comando;
+        // Adapter responsável por inserir dados em um dataTable
+        public static MySqlDataAdapter Adaptador;
+        // DataTable responsável por ligar o banco em controles com a propriedade DataSource
+        public static DataTable datTabela;
+
+        public static void AbrirConexao()
+        {
+            try
+            {
+                // Estabelece os parâmetros para a conexão com o banco
+                Conexao = new MySqlConnection("server=localhost;port=3307;uid=root;pwd=etecjau");
+
+                // Abre a conexao com o banco de dados
+                Conexao.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void FecharConexao()
+        {
+            try
+            {
+                // Fecha a conexão com o banco de dados
+                Conexao.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void CriarBanco()
+        {
+            try
+            {
+                //Chama a funcão para abertura de conexão com o banco
+                AbrirConexao();
+
+                //Informa a instrução SQL
+                Comando = new MySqlCommand("CREAT DATABASE IF NOT EXISTS vendas; USE vendas", Conexao);
+                // Executa a Query no MySQL (Raio de Workbanch)
+                Comando.ExecuteNonQuery();
+
+                Comando = new MySqlCommand("Create TABLE IF NOT EXISTS cidades " +
+                                           "(id integer auto_increment primary key, " +
+                                           "nome char(40)," +
+                                           "uf char(02))", Conexao);
+                Comando.ExecuteNonQuery();
+
+                // Chama a função para fechar a conexão com o banco
+                FecharConexao();
+            }
+            catch (Exception e) 
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void FrmMenu_Load (object sender, EventArgs e)
+        {
+            Banco.CriarBanco();
+        }
+    }
+}
--- 231052_231284/Program.cs
+++ 231052_231284/Program.cs
@@ -16,7 +16,7 @@ namespace _231052_231284
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmMenu());
         }
     }
 }
